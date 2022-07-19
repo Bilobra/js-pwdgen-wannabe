62ef168e0c7b3c96d73a4ef0cdd1a8716d41e51a
Bilobra <bra.carrozzo@gmail.com>
2022-07-19 15:18:22 +0200

    add variables

--- js/app.js
+++ js/app.js
@@ -8,3 +8,34 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+
+// NOME UTENTE
+// dichiarazione variabile
+
+let myName = prompt('Qual è il tuo nome?')
+
+console.log('nome utente: ' + myName)
+
+
+
+// COGNOME UTENTE
+// dichiarazione variabile
+let mySurname = prompt('Qual è il tuoo congome?')
+
+console.log('cognome utente: ' + mySurname)
+
+
+// COLORE PREFERITO
+// dichiarazione variabile
+let myColor = prompt('Qual è il tuo colore preferito?')
+
+console.log('colore preferito: ' + myColor)
+
+
+// NUMERO CASUALE
+
+let num = Math.round(Math.random() * 100)
+
+console.log(num)
+
+
